8c908aea4257d7cedbbedc1d1e99742747da5407
demeviktor02 <deme.viktor02@gmail.com>
2023-09-15 13:16:45 +0200

    SwitchSceneTries

---
+++ Unity code/Hero/Assets/Scripts/Scene/OnCollideChangeScene.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnCollideChangeScene : MonoBehaviour
+{
+    public string SceneName;
+    public GameManager gm;
+    public Vector2 otherSide;
+    private void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        gm.switchedScene = true;
+        GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>().position = otherSide;
+        SceneManager.LoadScene(SceneName, LoadSceneMode.Single);
+    }
+}
